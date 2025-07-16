b2d4e3561c69e0eda8e6bba94b53a2a437ec309c
JohnFarlander <omkar.purav@gmail.com>
2025-07-16 23:06:06 +0530

    df

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base :'/roy-nursery',
+  base: '/Nursery-react/', // ✅ MATCHES YOUR REPO NAME
   plugins: [react()],
-})
+});
