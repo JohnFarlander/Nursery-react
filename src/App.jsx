21fa773d6abdaae3f828c5db69b18be47fe5aa66
JohnFarlander <omkar.purav@gmail.com>
2025-07-18 23:45:15 +0530

    vb

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { CartProvider } from "./context/CartContext";
 import LandingPage from "./pages/LandingPage";
 import ProductsPage from "./pages/products";
 import CartPage from "./pages/CartPage";
+import Header from "./components/Header";
 import "./styles/styles.css";
 
 function App() {
   return (
     <CartProvider>
       <Router basename="/Nursery-react"> {/* 👈 Add basename here */}
+        <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<ProductsPage />} />
